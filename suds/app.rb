7857e4e39e603fe5d7570e295e0a292e0c934c09
devyn <devyn.cairns@gmail.com>
2008-10-26 08:56:31 -0700

    Suds/app ready.

--- suds/app.rb
+++ suds/app.rb
@@ -1,10 +1,19 @@
+require 'socket'
 module Suds
     class App
         def initialize(appname, &receiver)
+            @appname = appname
+            @receiver = receiver
+            @sock = TCPSocket.new('localhost', 42854)
+            @sock.puts "I am #@appname"
+            @thread = Thread.start{loop{receiver.call(@sock.readline)}}
         end
         def send(appname, msg)
+            @sock.puts "to #{appname}: #{msg}"
         end
         def disconnect
+            @thread.kill
+            @sock.close
         end
     end
 end
