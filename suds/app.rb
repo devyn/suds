f69dd9b11de0211306ec018befde2e32f17fbdd8
devyn <devyn.cairns@gmail.com>
2008-11-05 18:11:51 -0800

    okay, broke the code, hopefully fixed in next commit

--- suds/app.rb
+++ suds/app.rb
@@ -5,10 +5,10 @@ module Suds
             @appname = appname
             @receiver = receiver
             @sock = TCPSocket.new('localhost', 42854)
-            @sock.puts "I am #@appname"
-            @thread = Thread.start{loop{receiver.call(@sock.readline)}}
+            @sock.puts "logon: #@appname"
+            @thread = Thread.start{loop{sr = @sock.readline.scan(/^from ([A-Za-z0-9.-_]+): (.*)/)[0];receiver.call(sr[0], sr[1])}}
         end
-        def send(appname, msg)
+        def send(appname, msg) # You can also use "SYSWIDE" as the appname.
             @sock.puts "to #{appname}: #{msg}"
         end
         def disconnect
--- suds/daemon.rb
+++ suds/daemon.rb
@@ -1,6 +1,42 @@
+require 'socket'
+require 'thread'
 module Suds
+    class AppCollection < Array
+        def find appname
+            select {|app| app.name == appname}.first
+        end
+    end
     class Daemon
-        
+        attr_reader :thread
+        def initialize
+            @server = TCPServer.new("localhost", 42854) # The Suds port is always 42854
+            @apps   = AppCollection.new
+        end
+        def start
+            @thread = Thread.start do
+                sock = @server.accept
+                @apps << App.new(sock, @apps)
+            end
+        end
+        def stop
+            @thread.kill
+            @apps.each do |app|
+                app.kill_thread
+            end
+            @apps = AppCollection.new; GC.start # clears the apps
+        end
+    end
+    class App
+        attr_accessor :name
+        def initialize(socket, appcol, appname=nil)
+            @name = appname
+            @sock = socket
+            @lock = Mutex.new
+            @thrd = Thread.start { loop { @
+        end
+        def lock(&blk)
+            @lock.synchronize &blk
+        end
     end
 end
 
